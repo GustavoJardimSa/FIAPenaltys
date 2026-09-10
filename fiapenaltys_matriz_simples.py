daff24e5551675108c9551d5c3e4beed356ca12d
pivattidev <alepivatti@gmail.com>
2026-09-10 18:05:01 -0300

    Game system complete.

--- fiapenaltys_matriz_simples.py
+++ fiapenaltys_matriz_simples.py
@@ -125,8 +125,7 @@ def definir_resultado(chance_defesa):
 
 def mostrar_placar(time_1, gols_time_1, time_2, gols_time_2):
     print('\n==============================')
-    print('            PLACAR')
-    print('==============================')
+    print('       PLACAR')
     print(f'{time_1}: {gols_time_1}')
     print(f'{time_2}: {gols_time_2}')
     print('==============================\n')
@@ -186,92 +185,125 @@ def main():
     gols_time_1 = 0
     gols_time_2 = 0
 
-    #jogo principal
+    historico_time_1 = ['-', '-', '-', '-', '-']
+    historico_time_2 = ['-', '-', '-', '-', '-']
+
+    #JOGO PRINCIPAL
     for rodada in range(1,6):
-
         os.system('cls')
+
         print(f'\n========== RODADA {rodada} DE 5 ==========')
-
-        executar_cobranca(gol, time_1, time_2)
-        executar_cobranca(gol, time_2, time_1)
-
         mostrar_placar(
-            time_1, gols_time_1,
-            time_2, gols_time_2
+            time_1, ''.join(historico_time_1),
+            time_2, ''.join(historico_time_2)
         )
 
-        resultado = executar_cobranca(
-            gol, time_1, time_2
-        )
+        #COBRANÇA DO TIME 1
+        resultado = executar_cobranca(gol, time_1, time_2)
 
         if resultado == 'GOL':
             gols_time_1 += 1
+            historico_time_1[rodada - 1] = 'O'
+        else:
+            historico_time_1[rodada - 1] = 'X'
 
         os.system('cls')
 
+        print(f'\n========== RODADA {rodada} DE 5 ==========')
         mostrar_placar(
-            time_1, gols_time_1,
-            time_2, gols_time_2
+            time_1, ''.join(historico_time_1),
+            time_2, ''.join(historico_time_2)
         )
 
-        resultado = executar_cobranca(
-            gol, time_2, time_1
-        )
+        #COBRANÇA DO TIME 2
+        resultado = executar_cobranca(gol, time_2, time_1)
 
         if resultado == 'GOL':
             gols_time_2 += 1
+            historico_time_2[rodada - 1] = 'O'
+        else:
+            historico_time_2[rodada - 1] = 'X'
 
-    #resultado após jogo
-    os.system('cls')
+        os.system('cls')
 
-    mostrar_placar(
-        time_1, gols_time_1,
-        time_2, gols_time_2
-    )
-# alternadas
+        print(f'\n========== RODADA {rodada} DE 5 ==========')
+        mostrar_placar(
+            time_1, ''.join(historico_time_1),
+            time_2, ''.join(historico_time_2)
+        )
+
+        if rodada < 5:
+            input('Pressione ENTER para iniciar a próxima rodada.')
+
+# COBRANÇAS ALTERNADAS
     if gols_time_1 == gols_time_2:
        print('DEU EMPATE! QUE JOGO EMOCIONANTE!')
        print('AGORA VAMOS PARA AS COBRANÇAS ALTERNADAS') 
+       input('Pressione ENTER para continuar.')
 
        rodada_alternada = 1
 
        while gols_time_1 == gols_time_2:
             print(f'\n===== RODADA ALTERNADA {rodada_alternada} =====')
-
-            resultado = executar_cobranca(
-                gol, time_1, time_2
+            mostrar_placar(
+                time_1, ''.join(historico_time_1),
+                time_2, ''.join(historico_time_2)
             )
+
+            #COBRANÇA DO TIME 1
+            resultado = executar_cobranca(gol, time_1, time_2)
 
             if resultado == 'GOL':
                 gols_time_1 += 1
+                historico_time_1.append('O')
+            else:
+                historico_time_1.append('X')
 
-            resultado = executar_cobranca(
-                gol, time_2, time_1
+            os.system('cls')
+
+            print(f'\n===== RODADA ALTERNADA {rodada_alternada} =====')
+            mostrar_placar(
+                time_1, ''.join(historico_time_1),
+                time_2, ''.join(historico_time_2)
             )
+
+            #COBRANÇA DO TIME 2
+            resultado = executar_cobranca(gol, time_2, time_1)
             
             if resultado == 'GOL':
                 gols_time_2 += 1
+                historico_time_2.append('O')
+            else:
+                historico_time_2.append('X')
 
+            os.system('cls')
+
+            print(f'\n===== RODADA ALTERNADA {rodada_alternada} =====')
             mostrar_placar(
-                time_1, gols_time_1,
-                time_2, gols_time_2
+                time_1, ''.join(historico_time_1),
+                time_2, ''.join(historico_time_2)
             )
 
-            rodada_alternada += 1
+            if gols_time_1 == gols_time_2:
+                rodada_alternada += 1
+                input('Pressione ENTER para a próxima rodada alternada.')
 
-  # definição do vencedor
+    #RESULTADO FINAL
+    os.system('cls')
+
     print('\n==============================')
     print('        FIM DE JOGO')
     print('==============================')
 
     mostrar_placar(
-        time_1, gols_time_1,
-        time_2, gols_time_2
+        time_1, ''.join(historico_time_1),
+        time_2, ''.join(historico_time_2)
     )
+
+    print(f'Placar em gols: {gols_time_1} x {gols_time_2}')
 
     if gols_time_1 > gols_time_2:
         print(f'{time_1} SAI VENCEDOR!')
-
     else:
         print(f'{time_2} SAI VENCEDOR!')
 
